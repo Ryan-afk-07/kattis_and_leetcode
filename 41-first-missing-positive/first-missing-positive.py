0a6449152d40cb753e1b8ad38a4ade70d87e2af5
Ryan Fong <rfong0001@gmail.com>
2025-10-12 19:01:21 +0800

    Time: 34 ms (70.77%) | Memory: 29.4 MB (29.30%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -6,6 +6,7 @@ class Solution:
         1. It is alright to sort the list and remove all negative integers wtf
         2. Check if the list is empty. If it is means all elements in the list is negative. Return 1
         3. If the list is not. Initiate a pointer that starts with the first value. If the pointer is strictly greater, return the target, if not, add pointer with 1 and add target with 1 and continue on 
+        Things to keep note: Need to amend nums directly to preserve space complexity. And DO A FOR LOOP, NOT WHILE since there may be duplicate integers. If the integer is there, even if its a duplicate, iterating to the next same value will make your integer the same until it reaches the next sorted value
         """
         #remove negative numbers
         nums = [i for i in nums if i > 0]
