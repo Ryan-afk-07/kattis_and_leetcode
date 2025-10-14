0480afea2a895ca18891c401f47d97eef3b1fca7
Ryan Fong <rfong0001@gmail.com>
2025-10-15 02:09:31 +0800

    Time: 19 ms (94.59%) | Memory: 18.7 MB (38.50%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.py
@@ -0,0 +1,93 @@
+class Solution:
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        """
+        Idea: Do a combination of all variations of the list. And then have them all combined.
+        OH then sliding window since the result of all combinations will ALL have the same length. They must all BE CONCATENATED.
+        
+        #I LOVEEEEEE BACKTRACK SHIT
+        exists = []
+        def backtrack(ind, wordss, combi):
+            if wordss == []:
+                return
+            
+            while ind < len(wordss):
+                wordsy = wordss
+                backtrack(ind + 1, wordsy, combi)
+                combi = combi + [wordsy[ind]]
+                wordsy = wordss[:ind] + wordss[ind+1:]
+                backtrack(0, wordsy, combi)
+                if len(combi) == len(words) and combi not in exists:
+                    exists.append(combi)
+                    final.append(combi)
+                    break
+                else:
+                    break
+
+        
+        final = []
+        backtrack(0, words, [])
+        for i in range(len(final)):
+            final[i] = "".join(final[i])
+
+        print(final)
+
+        window = len(final[0])
+        pointer = 0
+        ind = []
+        while pointer < len(s) - window + 1:
+            string = s[pointer:pointer+window]
+            print(string)
+            if string in final:
+                ind.append(pointer)
+                pointer += 1
+            else:
+                pointer += 1
+        return ind
+    """
+        """
+        Updated: my solution MAY work, but only if GPU is solid. Need a more efficient solution
+        FROM solutions page - retrieving understanding.
+        1. Finding permutations: since ALL STRINGS are of the same length in the words list. There is NO need to do permutations from the words list. Do the REVERSE INSTEAD.
+        2. Do a sliding windoe for each section of the string. The criteria for a permutation will be valid IF:
+        2.1 current window has all the words from the words array
+        2.2 Count of each of the words in the window is the same as the count in the array [HENCE the hashmap!]
+        3. Do 2 hashmaps. 1 will contains the count of each word in the words list.
+        Another one will keep the count of words in the current window.
+        """
+        #keep track of number of words in given words list
+        words_hash = {}
+        #keep track of number of words and type of words in the current window of string
+        wordSize = len(words[0])
+        window = wordSize * len(words)
+        word_count = len(words)
+        for i in words:
+            words_hash[i] = words_hash.get(i,0) + 1
+        ans = []
+        print(words_hash)
+        for offset in range(wordSize):
+            start = offset
+            currentCount = {}
+            count = 0
+            for end in range(offset, len(s) - wordSize + 1, wordSize):
+                curr_word = s[end:end + wordSize]
+                if curr_word in words_hash:
+                    currentCount[curr_word] = currentCount.get(curr_word, 0) + 1
+                    count += 1
+
+                    while currentCount[curr_word] > words_hash[curr_word]:
+                        start_word = s[start:start + wordSize]
+                        currentCount[start_word] -= 1
+                        start += wordSize
+                        count -= 1
+
+                    if count == word_count:
+                        ans.append(start)
+                else:
+                    count = 0
+                    start = end + wordSize
+                    currentCount.clear()
+        return ans
+                
+
+            
+
