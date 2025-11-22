862020849e61f46dbec04b8a8607ac241750af10
Ryan Fong <rfong0001@gmail.com>
2025-11-23 02:55:05 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (51.04%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        if not s or s[0] == "0":
+            return 0
+
+        end = len(s)
+        memo = {}
+
+        def dfs(start):
+            if start == end:
+                return 1  # one valid decode path
+        
+            if start in memo:
+                return memo[start]
+        
+            if s[start] == "0":
+                return 0  # can't decode starting with 0
+        
+            ways = dfs(start + 1)  # take 1 digit
+        
+            # take 2 digits only if it's valid (<= 26)
+            if start + 1 < end and int(s[start:start+2]) <= 26:
+                ways += dfs(start + 2)
+        
+            memo[start] = ways
+            return ways
+        return dfs(0)
